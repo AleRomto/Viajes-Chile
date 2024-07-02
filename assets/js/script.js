9e6b5f204b04c4ea7fef15ffccf1e964bfc33c8b
Ale <[alexiaromeropinto@gmail.com]>
2024-07-01 23:09:21 -0400

    Se adicionan imágenes, se cambian vistas en el index y se trabajan sobre las cards de destacados. Se implementa tooltips

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,40 @@
-$(function(){
+// Smooth
+document.querySelectorAll('a.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Boton reservar
+let btnEnviarAlCorreo = document.getElementById('reservar'); 
+
+// Función boton Enviar
+$(function() {
+   
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
     tooltipTriggerList.map(function (tooltipTriggerEl) {
         return new bootstrap.Tooltip(tooltipTriggerEl);
     });
 
-    $("#enviarMensaje").on('click', function(){
-        alert("Hemos recibido tu respuesta");
+ // Evento de envío del formulario
+    document.getElementById('contactForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        var nombre = document.getElementById('name').value;
+        var correo = document.getElementById('email').value;
+        var mensaje = document.getElementById('message').value;
+
+ // Valideishon
+        if (nombre === "" || correo === "" || mensaje === "") {
+            alert("Por favor, completa todos los campos");
+            return;
+        }
+
+ // Alerta de envío exitoso
+        alert('El mensaje fue enviado correctamente');
     });
 });
